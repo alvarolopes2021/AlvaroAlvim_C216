3db5cfd03f17962d707657d4507a6991b40fbd7c
alvaro <alvarolopes2020@protonmail.com>
2022-09-24 12:52:55 -0300

    feat: added new tests

--- LABS SD/lab_rest_server/src/test/java/br/inatel/labs/lab_rest_server/controller/CursoControllerTest.java
+++ LABS SD/lab_rest_server/src/test/java/br/inatel/labs/lab_rest_server/controller/CursoControllerTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import br.inatel.labs.lab_rest_server.model.Curso;
+
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CursoControllerTest {
@@ -22,6 +24,50 @@ class CursoControllerTest {
 		.exchange()
 		.expectStatus().isOk()
 		.expectBody().returnResult(); //retornou alguma coisa
+	}
+	
+	@Test
+	void dadoCursoIdValido_quandoGetCursoPorId_entaoRespondeComCursoValido() {
+		Long cursoIdValido = 1L;
+		
+		Curso response = webTestClient.get()
+		.uri("/curso/search/" + cursoIdValido)
+		.exchange()
+		.expectStatus().isOk()
+		.expectBody(Curso.class)
+			.returnResult()
+			.getResponseBody(); //retornou alguma coisa
+		
+		assertNotNull(response);
+		
+		assertEquals(response.getId(), cursoIdValido);	
+		
+	}
+	
+	@Test
+	void dadoCursoIdInvalido_quandoGetCursoPorId_entaoRespondeComStatusNotFound() {
+		Long cursoIdValido = 99L;
+		
+		webTestClient.get()
+		.uri("/curso/search/" + cursoIdValido)
+		.exchange()
+		.expectStatus().isNotFound();
+		
+	}
+	
+	@Test
+	void dadoNovoCurso_quandoPostCurso_entaoRespondeComStatusCreatedECursoValido() {
+		Curso curso = new Curso(4L,"Testando REST com Spring WebFlux", 120 );
+		
+		Curso cursoResponse = webTestClient.post()
+		.uri("/curso/create")
+		.bodyValue(curso)
+		.exchange()
+		.expectStatus().isCreated()
+		.expectBody(Curso.class).returnResult().getResponseBody();
+		
+		assertNotNull(cursoResponse);
+		
 	}
 
 }
